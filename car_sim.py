2761f41da99ff669095da9a9ad3b39e604367afc
Najam Syed <najam.r.syed@gmail.com>
2019-04-28 00:36:54 -0400

    Update car_sim.py with sample simulation

--- car_sim.py
+++ car_sim.py
@@ -1,4 +1,3 @@
-rom scipy import interpolate
 from car import Car
 from road import Road
 
@@ -11,15 +10,31 @@ def simulate(time_step=0.0002):
     # wheel point of contact (x = l_f). Choose road limits (x_min, x_max)
     # accordingly.
     road_limits = (-2 * car.properties["l_r"], 2 * car.properties["l_f"])
-    road = Road(length=road_limits[1] - road_limits[0])
+    road_length = road_limits[1] - road_limits[0]
+    road = Road(length=road_length, x_min=road_limits[0])
+    car.set_road_func(road)
 
     elapsed_time = 0
 
-    # Initial velocity and acceleration.
+    # Initial horizontal velocity and acceleration.
 
     while True:
         ######################################################################
         ######################################################################
-        # SET THE DESIRED SIMULATION PARAMETERS HERE.
+        ########### SET THE DESIRED SIMULATION PARAMETERS HERE ###############
+        if 0 <= elapsed_time < 2:
+            car.set_accel(4)
+        elif 2 <= elapsed_time < 4:
+            car.set_accel(-5)
+        elif 4 <= elapsed_time <= 5:
+            car.set_accel(4)
+        else:
+            break
+        ######################################################################
+        ######################################################################
 
-        if elapsed
+        car.update_state(time_step)
+        elapsed_time += time_step
+
+if __name__ == "__main__":
+    simulate()
