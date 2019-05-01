4b072480fd71f1da8afed3f01e472e94970b7586
Najam Syed <najam.r.syed@gmail.com>
2019-04-30 22:26:59 -0400

    Make package

---
+++ halfcar/__init__.py
@@ -0,0 +1,3 @@
+from .car import Car
+from .road import Road
+from .plot_sim import PlotSim
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="halfcar",
+    version="2.0.0",
+    description="Half-car dynamic model simulation",
+    url="https://github.com/nrsyed/half-car",
+    author="Najam R Syed",
+    author_email="najam.r.syed@gmail.com",
+    license="GPL",
+    packages=["halfcar"],
+    install_requires=[
+        "numpy==1.15.1",
+        "scipy",
+        "matplotlib"
+    ]
+)
